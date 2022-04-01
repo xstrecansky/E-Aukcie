4f5b53d6cc552453a9001014374925331812c3e9
Maximilián Strečanský <98816862+xstrecansky@users.noreply.github.com>
2022-04-01 22:04:41 +0200

    Add files via upload

--- items/ItemClass.java
+++ items/ItemClass.java
@@ -1,5 +1,7 @@
 package items;
 
+import java.util.ArrayList;
+
 import users.*;
 
 public class ItemClass {
@@ -7,14 +9,14 @@ public class ItemClass {
     private UserClass owner;
     private boolean sold;
     private int highestOffer;
-    //Contructor
-    ItemClass(){
-    }
-    public ItemClass(String name, UserClass owner, boolean sold, int highestOffer){
+    private UserClass hOfferOwner;
+
+    public ItemClass(String name, UserClass owner, boolean sold, int highestOffer, UserClass hOfferOwner){
         this.name = name;
         this.owner = owner;
         this.sold = sold;
         this.highestOffer = highestOffer;
+        this.hOfferOwner = hOfferOwner;
     }
     public String getItemName(){
         return name;
@@ -27,15 +29,42 @@ public class ItemClass {
     }
     //Zmenime majitela -> produkt bol predany cize zmenime
     //aj premennu o predaji
-    public void changeOwner(UserClass newOwner){
-        this.owner = newOwner;
+    public void changeOwner(){
+        this.owner = this.hOfferOwner;
         this.sold = true;
-        return;
+        
     }
     //Dostaneme ponuku a ak je vacsia ako momentalna ponuka, zmenime najvacsiu ponuku
-    public void getOffer(int offer){
-        if(offer>this.highestOffer)
-            this.highestOffer=offer;
-        return;
+    public void getOffer(int offer, UserClass OfferOwner){
+        if(offer>this.highestOffer){
+            this.highestOffer = offer;
+            this.hOfferOwner = OfferOwner;
+        }
+    }
+    //Funkcia nam prehlada vsetky predmety v databaze a vypise tie co vlastni dany pouzivatel
+    public static String getOwnedItems(UserClass user, ArrayList<ItemClass> database) {
+        String returnedText ="";
+        for(ItemClass loopItem: database){
+            if(loopItem.getOwner().getName()==user.getName())
+                returnedText = returnedText+(loopItem.getItemName()+"\n");
+            
+        }
+        return returnedText;
+    }
+    //Funkcia nam prehlada vsetky predmety v databaze a vypise tie ktore su momentalne v aukcii
+    public static String getItemsOnSale(ArrayList<ItemClass> database) {
+        String returnedText ="";
+        for(ItemClass loopItem : database){
+            if(loopItem.isSold()==false)
+                returnedText = returnedText+(loopItem.getItemName()+"\n");
+        }
+        return returnedText;
+    }
+    //Zmenime majitelov na majitelov najvacsej ponuky
+    public static ArrayList<ItemClass> endSales(ArrayList<ItemClass> database){
+        for(ItemClass loopItem : database){
+            loopItem.changeOwner();
+        }
+        return database;
     }
 }
